d17cab3114f4dc50313df9f48ac63cbc9f454880
Tristan de Guillebon <tdeguill@gmail.com>
2021-12-29 18:12:42 +0100

    Update route Stocks

--- routes/index.js
+++ routes/index.js
@@ -18,22 +18,59 @@ router.get("/stocks/:token/:days/:myCryptos", async function (req, res) {
   ];
 
   if (user) {
-    let ownedCryptos = [...user.ownedCryptos];
-    if (ownedCryptos) {
-      let ids = "";
-      for (let i = 0; i < ownedCryptos.length; i++) {
-        ids += ownedCryptos[i].id + ",";
-      }
+    if (req.params.myCryptos === "true") {
+      let ownedCryptos = [...user.ownedCryptos];
+      if (ownedCryptos) {
+        let ids = "";
+        for (let i = 0; i < ownedCryptos.length; i++) {
+          ids += ownedCryptos[i].id + ",";
+        }
 
+        coinGeckoAPI
+          .get(
+            `/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=${maxStocks}&page=1&sparkline=false&price_change_percentage=24h%2C7d&ids=${ids}`
+          )
+          .then(async (response) => {
+            const cryptos = [];
+            for (let i = 0; i < response.data.length && i < maxStocks; i++) {
+              // On limite i à maxStocks pour éviter de surcharger le nombre de fetch à coinGecko
+              const price = await coinGeckoAPI.get(
+                `https://api.coingecko.com/api/v3/coins/${
+                  response.data[i].id
+                }/market_chart?vs_currency=eur&days=${
+                  req.params.days
+                }&interval=${
+                  intervalUpdate.find((e) => e.days == req.params.days).interval
+                }`
+              );
+              cryptos.push({
+                image: response.data[i].image,
+                name: response.data[i].name,
+                id: response.data[i].id,
+                currentPrice: response.data[i].current_price,
+                price_change_24h:
+                  response.data[i].price_change_percentage_24h_in_currency,
+                price_change_7d:
+                  response.data[i].price_change_percentage_7d_in_currency,
+                prices: price.data.prices,
+              });
+            }
+            Promise.all(cryptos).then((response) =>
+              res.json({ result: true, cryptos: response })
+            );
+          });
+      } else {
+        res.json({ result: false, message: "" });
+      }
+    } else {
       coinGeckoAPI
         .get(
-          `/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=${maxStocks}&page=1&sparkline=false&price_change_percentage=24h%2C7d` +
-            (req.params.myCryptos === "true" ? `&ids=${ids}` : "")
+          `/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=${maxStocks}&page=1&sparkline=false&price_change_percentage=24h%2C7d`
         )
         .then(async (response) => {
           const cryptos = [];
           for (let i = 0; i < response.data.length && i < maxStocks; i++) {
-            // On limite i à 20 pour éviter de surcharger le nombre de fetch à coinGecko
+            // On limite i à maxStocks pour éviter de surcharger le nombre de fetch à coinGecko
             const price = await coinGeckoAPI.get(
               `https://api.coingecko.com/api/v3/coins/${
                 response.data[i].id
@@ -58,6 +95,8 @@ router.get("/stocks/:token/:days/:myCryptos", async function (req, res) {
           );
         });
     }
+  } else {
+    res.json({ result: false, message: "No user found in db" });
   }
 });
 
